8c138828b14594de2ec30a9e9b3f7c9e1ee66662
PandePraditya <pandepraditya@gmail.com>
2025-03-15 00:54:24 +0800

    feat: added routes for UserController and AuthController

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Check the server if its running or not
+Route::get('health', function () {
+    return response()->json(['message' => 'Server is running!'], 200);
+});
+
+// Public Route for Authentication
+Route::post('login', [AuthController::class, 'login']);
+
+// Authenticated routes
+Route::middleware('auth:sanctum')->group(function () {
+    // Route for Authentication
+    Route::controller(AuthController::class)->group(function () {
+        Route::post('register', 'register');
+        Route::post('logout', 'logout');
+    });
+    
+    // Route for User
+    Route::controller(UserController::class)->group(function () {
+        // Route for getting all users
+        Route::get('user-list', 'index');
+        // Route for getting user details
+        Route::get('users', 'getUserDetails');
+
+        // Route for Soft Delete and Restore a user
+        Route::delete('users/{id}', 'destroy');
+        Route::post('users/{id}/restore', 'restore');
+    });
 });

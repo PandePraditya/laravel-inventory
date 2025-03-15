081c7a85d7fe614231332e3da985f51edf42561c
PandePraditya <pandepraditya@gmail.com>
2025-03-16 00:49:21 +0800

    feat: added Inventory feature and added a middleware for admin

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\AuthResource;
+use App\Http\Resources\UserResource;
 use App\Models\User;
-// use Illuminate\Http\Request;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -42,19 +43,45 @@ class AuthController extends Controller
     public function register(RegisterRequest $request)
     {
         $credentials = $request->validated();
+        // Hash the password
+        $credentials['password'] = Hash::make($credentials['password']);
 
-        $user = User::create([
-            'full_name' => $credentials['full_name'],
-            'email' => $credentials['email'],
-            'phone_number' => $credentials['phone_number'],
-            'password' => Hash::make($credentials['password']),
-            'role_id' => $credentials['role_id'],
-            'division_id' => $credentials['division_id'],
-        ]);
+        $user = User::create($credentials);
 
         return response()->json([
             'message' => 'User registered successfully',
-            'user' => new AuthResource($user), // Use a resource for consistent formatting
+            'user' => new UserResource($user)
         ], 201);
+    }
+
+    // Logout a user
+    public function logout(Request $request)
+    {
+        // Get the token from the request
+        $token = $request->bearerToken();
+
+        // Check if the token is provided
+        if (!$token) {
+            return response()->json([
+                'message' => 'Sorry, you must login first.',
+            ], 400);
+        }
+
+        // Get the authenticated user using Sanctum
+        $user = $request->user();
+
+        // If user isn't authenticated, return unauthorized
+        if (!$user) {
+            return response()->json([
+                'message' => 'Unauthorized. Token is invalid or user not found.',
+            ], 401);
+        }
+
+        // Revoke the current token
+        $user->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out successfully.'
+        ]);
     }
 }
